18cde2b5d5b0551f5157ebaeb975cfb1303eae38
Zubayer Ahamed <zubayerahamed.freelancer@gmail.com>
2022-07-23 23:27:58 +0600

    1st commit

---
+++ src/main/java/com/kit/pos/KitposApplication.java
@@ -0,0 +1,13 @@
+package com.kit.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitposApplication.class, args);
+	}
+
+}
