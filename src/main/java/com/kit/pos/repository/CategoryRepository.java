3a5d1b97a1ddcf8252a1277fe6539d9195541a0a
Zubayer Ahamed <zubayerahamed.freelancer@gmail.com>
2022-08-04 15:17:01 +0600

    d

---
+++ src/main/java/com/kit/pos/repository/CategoryRepository.java
@@ -0,0 +1,16 @@
+package com.kit.pos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kit.pos.entity.Category;
+import com.kit.pos.entity.pk.CategoryPK;
+
+/**
+ * @author Zubayer Ahamed
+ * @since Aug 4, 2022
+ */
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, CategoryPK> {
+
+}
---
+++ src/main/java/com/kit/pos/service/CategoryService.java
@@ -0,0 +1,20 @@
+package com.kit.pos.service;
+
+import com.kit.pos.dto.request.CategoryRequestDTO;
+import com.kit.pos.dto.response.CategoryResponseDTO;
+import com.kit.pos.util.Response;
+
+/**
+ * @author Zubayer Ahamed
+ * @since Aug 4, 2022
+ */
+public interface CategoryService {
+
+	public Response<CategoryResponseDTO> findByCategoryName(String name);
+
+	public Response<CategoryResponseDTO> save(CategoryRequestDTO reqDto);
+
+	public Response<CategoryResponseDTO> update(CategoryRequestDTO reqDto);
+
+	public Response<CategoryResponseDTO> getAllCategories();
+}
